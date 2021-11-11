d86575ef173737dfeee03c696f939a21869bef82
IrmaTheLove <84366605+IrmaTheLove@users.noreply.github.com>
2021-11-11 09:48:21 +0700

    Tugas 2
    Tugas 2

---
+++ Tugas 2/Tugas2.js
@@ -0,0 +1,11 @@
+var nama = "Angga";
+var umur = 19;
+var menikah = false;
+var pacar = null;
+var teman = [Dana, Miftah, Ibnu, Irja, Ganang];
+
+console.log(nama);
+console.log(umur);
+console.log(menikah);
+console.log(pacar);
+console.log(teman);
